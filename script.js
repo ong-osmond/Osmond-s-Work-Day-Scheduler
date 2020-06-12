bfd0fd94c9e7759c362c16f4e262a792b628654d
ong_osmond <ong.osmond@yahoo.com>
2020-06-12 11:48:11 +0800

    Made use of jQuery to simplify adding elements to the page.

--- script.js
+++ script.js
@@ -1,36 +1,64 @@
-$(document).ready(function () {
+$(document).ready(function() {
     $("#currentDay").text(moment().format('dddd' + ", " + 'MMMM Do'));
 
     //Create timeblocks
-
     var now = moment();
     console.log(now.startOf('day').format('hh a'));
 
     var hours = [];
-    var hoursIDs = [];
-    var past = [];
-    var future = [];
     var timeNow = (moment().hour());
     console.log(timeNow);
     for (var i = now.startOf('day').hour(); i < 24; i++) {
         //Create timeblock and add to the container
-        var timeBlockRow = `<row class=time-block id=time-block${[i]}></row>`;
+        var timeBlockRow = $("<row>");
+        timeBlockRow.addClass("time-block");
+        timeBlockRow.attr("id", "time-block-" + i);
+
         $(".container").append(timeBlockRow);
+
         //Add hour to the hours array
         hours.push(i + ":00");
+
         //Append a div class=hour to the timeblock
-        var hourColumn = `<div class=col-3 id=time${[i]}>${hours[i]}</div>`
-        $(`#time-block${[i]}`).append(hourColumn);
-        $(`#time${[i]}`).addClass("hour");
+        var hour = $("<div>");
+        hour.addClass("col-3");
+        hour.attr("id", "hour-" + i);
+        $("#time-block-" + i).append(hour);
+        hour.text(hours[i]);
+
+        //Append a text area for user to type in or edit a task
+        var task = $("<textarea>");
+        $("#time-block-" + i).append(task);
         //Compare the div class=hour value to the current time
         if (i < timeNow) {
-            $(`#time-block${[i]}`).append(`<textarea class = past id=text${[i]}>Past</textarea>`);
+            task.addClass("past");
         } else if (i == timeNow) {
-            $(`#time-block${[i]}`).append(`<textarea class = present id=text${[i]}>Present</textarea>`);
+            task.addClass("present");
+        } else {
+            task.addClass("future");
         }
-        else $(`#time-block${[i]}`).append(`<textarea class = future  id=text${[i]}>Future</textarea>`);
-        $(`#text${[i]}`).addClass("col-6");
+        task.attr("task", i);
+        var taskID = "task-ID" + i;
+        task.text(localStorage.getItem(taskID));
 
-        hoursIDs.push(i);
+        //Append buttons for each task
+        var button = $("<button>");
+        $("#time-block-" + i).append(button);
+        button.text("Save Task");
+        button.addClass("saveBtn")
+        button.attr("button-id", i);
     }
+
+    //Add button handling
+    $(".saveBtn").on("click", saveTask);
+
+    function saveTask() {
+        event.preventDefault();
+        //Get the task ID of the text field
+        var taskID = $(this.previousElementSibling.attributes.task).val();
+        var taskText = $(this.previousElementSibling).val();
+        localStorage.setItem("task-ID" + taskID, taskText);
+    }
+
+
 });
